b9a9b29b3078f6b846c56b5311835e6507a142a9
Jade <barre@LAPTOP-28UEV8BT>
2020-06-08 21:42:25 +0200

    Test d'un accroissement de la qualité x2 pour les biens Backstage Passes quand sellIn est entre 10 et 5 et qualité x3 quand sellin est inférieure à 5 + ajout de tests de baisse de la qualité pour les conjured Items de -4 quand la valeur SellIn est inférieure à 0 et de -2 quand la valeur SellIn est positive

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -18,15 +18,15 @@ describe('Aged Brie items update of quality', function () {
     it('should the quality increase', function() {
         const gildedRose = new GildedRose([   new Item("Aged Brie", 2, 2) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(3);
         expect(items[0].sellIn).to.equal(1);
+        expect(items[0].quality).to.equal(3);
     });
 
     it('should the quality stop increasing when it is 50', function() {
         const gildedRose = new GildedRose([   new Item("Aged Brie", 2, 50) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(50);
         expect(items[0].sellIn).to.equal(1);
+        expect(items[0].quality).to.equal(50);
     });
 
 });
@@ -34,47 +34,49 @@ describe('Aged Brie items update of quality', function () {
 
 describe('Backstage passes to a TAFKAL80ETC concert update of quality', function () {
 
-    it('should the quality increase', function() {
+    it('should the quality increase by 3 when sellIn < 5', function() {
         const gildedRose = new GildedRose([   new Item("Backstage passes to a TAFKAL80ETC concert", 4, 10) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(13);
         expect(items[0].sellIn).to.equal(3);
+        expect(items[0].quality).to.equal(13);
+    });
+
+    it('should the quality increase by 2 when sellIn < 10', function() {
+        const gildedRose = new GildedRose([   new Item("Backstage passes to a TAFKAL80ETC concert", 10, 20) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(9);
+        expect(items[0].quality).to.equal(22);
     });
 
     it('should the quality decrease to 0', function() {
         const gildedRose = new GildedRose([   new Item("Backstage passes to a TAFKAL80ETC concert", 0, 10) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(0);
         expect(items[0].sellIn).to.equal(-1);
+        expect(items[0].quality).to.equal(0);
     });
 });
 
 describe('Conjured items', function () {
 
-    it('should the quality decrease', function() {
+    it('should the quality decrease by 2', function() {
         const gildedRose = new GildedRose([   new Item("Conjured", 3, 5) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(3);
         expect(items[0].sellIn).to.equal(2);
+        expect(items[0].quality).to.equal(3);
     });
 
+    it('should the quality decrease by 4 when the sellIn <0', function() {
+        const gildedRose = new GildedRose([   new Item("Conjured", -1, 10) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(-2);
+        expect(items[0].quality).to.equal(6);
+    });
 
     it('should the quality decrease but not be less than 0', function() {
         const gildedRose = new GildedRose([   new Item("Conjured", 5, 0) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(0);
         expect(items[0].sellIn).to.equal(4);
-    });
-
-});
-
-describe('Quality for Normal and Conjured items should degrades twice as fast', function () {
-
-    it('should the quality decrease', function() {
-        const gildedRose = new GildedRose([   new Item("normal", 0, 2) ]);
-        const items = gildedRose.updateQuality();
         expect(items[0].quality).to.equal(0);
-        expect(items[0].sellIn).to.equal(-1);
     });
 
 });
@@ -84,8 +86,15 @@ describe('Normal items update of quality', function () {
     it('should the quality decrease', function() {
         const gildedRose = new GildedRose([   new Item("normal", 15, 40) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(39);
         expect(items[0].sellIn).to.equal(14);
+        expect(items[0].quality).to.equal(39);
+    });
+
+    it('should the quality decrease twice as fast when sellIn <0 ', function() {
+        const gildedRose = new GildedRose([   new Item("normal", 0, 2) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].sellIn).to.equal(-1);
+        expect(items[0].quality).to.equal(0);
     });
 
 });
@@ -95,8 +104,8 @@ describe('Sulfuras items', function () {
     it('should the quality decrease', function() {
         const gildedRose = new GildedRose([   new Item("Sulfuras, Hand of Ragnaros", 15, 80) ]);
         const items = gildedRose.updateQuality();
-        expect(items[0].quality).to.equal(80);
         expect(items[0].sellIn).to.equal(14);
+        expect(items[0].quality).to.equal(80);
     });
 
 });
