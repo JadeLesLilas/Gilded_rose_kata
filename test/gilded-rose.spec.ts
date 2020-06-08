29c88b49f76dd5e4f3552d8044c456fef8d622c1
Jade <barre@LAPTOP-28UEV8BT>
2020-06-08 21:16:36 +0200

    Test s'assurant que la qualité ne soit pas inférieure à 0 pour les 'Conjured Goods' + que la qualité se dégrade deux fois plus vite une fois la valeur SellIn négative pour les biens du type 'Conjured' & normaux

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -56,6 +56,28 @@ describe('Conjured Goods update of quality', function () {
 
 });
 
+describe('Quality for Conjured Goods should not be less than 0', function () {
+
+    it('should the quality decrease', function() {
+        const gildedRose = new GildedRose([   new Item("Conjured", 5, 0) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+        expect(items[0].sellIn).to.equal(4);
+    });
+
+});
+
+describe('Quality for Normal and Conjured Goods should degrades twice as fast', function () {
+
+    it('should the quality decrease', function() {
+        const gildedRose = new GildedRose([   new Item("normal", 0, 2) ]);
+        const items = gildedRose.updateQuality();
+        expect(items[0].quality).to.equal(0);
+        expect(items[0].sellIn).to.equal(-1);
+    });
+
+});
+
 describe('Normal Goods update of quality', function () {
 
     it('should the quality decrease', function() {
