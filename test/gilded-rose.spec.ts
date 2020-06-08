4ce20ba99b146a69f0e05747fae016aecc35c18d
Jade <barre@LAPTOP-28UEV8BT>
2020-06-08 19:03:40 +0200

    premier test d'ajout d'un item

--- test/gilded-rose.spec.ts
+++ test/gilded-rose.spec.ts
@@ -1,12 +1,14 @@
 import { expect } from 'chai';
-import { Item, GildedRose } from '../app/gilded-rose';
+import { GildedRose } from '../app/gilded-rose';
+import { Item } from '../app/item_model';
 
 describe('Gilded Rose', function () {
 
-    it('should foo', function() {
+    it('should add a new Item', function() {
         const gildedRose = new GildedRose([ new Item('foo', 0, 0) ]);
-        const items = gildedRose.updateQuality();
-        expect(items[0].name).to.equal('fixme');
+        expect(gildedRose.items[0].name).to.equal('foo');
+        expect(gildedRose.items[0].sellIn).to.equal(0);
+        expect(gildedRose.items[0].quality).to.equal(0);
     });
 
 });
